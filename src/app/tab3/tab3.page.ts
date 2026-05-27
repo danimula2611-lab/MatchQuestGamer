e66f4b483c2a04f4e8b98a530d2b17bc9bc65c4d
danimula2611-lab <danimula2611@gmail.com>
2026-05-27 11:33:07 +0200

    Fix: Alerta de cierre de sesion

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'; // <-- Importamos OnInit
+import { Component, OnInit } from '@angular/core'; 
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-tab3',
@@ -6,18 +7,39 @@ import { Component, OnInit } from '@angular/core'; // <-- Importamos OnInit
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page implements OnInit { // <-- Añadimos implements OnInit
+export class Tab3Page implements OnInit { 
 
-  // Variable estrella para controlar el esqueleto en tu perfil
+  // Variable para controlar el esqueleto de carga
   cargando: boolean = true;
 
-  constructor() {}
+  // FIJATE AQUÍ: Esta es la variable que necesita el HTML para pintar los botones de la alerta sin errores
+  botonesAlert = [
+    {
+      text: 'Cancelar',
+      role: 'cancel'
+    },
+    {
+      text: 'Sí, salir',
+      role: 'confirm',
+      handler: () => {
+        this.cerrarSesion(); // Si confirma, llamamos a la redirección
+      }
+    }
+  ];
+
+  // Inyectamos el router en el constructor
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    // Simulamos la carga de los datos del perfil del usuario
+    // Simulamos la carga de los datos del perfil
     setTimeout(() => {
       this.cargando = false;
     }, 1500);
+  }
+
+  // Función que te teletransporta al Login
+  cerrarSesion() {
+    this.router.navigate(['/login']);
   }
 
 }
